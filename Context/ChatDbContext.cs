2068971e698857f570465ab431822ccfb4aac6e3
Hforna <henriqueflashpingo@gmail.com>
2025-08-29 19:53:56 -0300

    configure entity framework for persist user data

---
+++ Context/ChatDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Scaffold.Web.Context
+{
+    public class ChatDbContext : IdentityDbContext
+    {
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Scaffold.Web.Models
+{
+    public class UserModel : IdentityUser
+    {
+         
+    }
+}
